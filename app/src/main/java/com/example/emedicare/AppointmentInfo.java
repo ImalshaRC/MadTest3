511bd0b59724cd6fc047e492ee30f05efa4725e8
Muditha0829 <muditha1gms@gmail.com>
2022-05-04 23:07:20 +0530

    User Modifying

---
+++ app/src/main/java/com/example/emedicare/AppointmentInfo.java
@@ -0,0 +1,14 @@
+package com.example.emedicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_info);
+    }
+}
