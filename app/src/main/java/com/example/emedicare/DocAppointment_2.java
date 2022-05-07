3c0007870e41c897c31b3f9f9aee3596dd326583
ImalshaRC <chaminduimalsha@gmail.com>
2022-05-07 12:45:53 +0530

    commit

--- app/src/main/java/com/example/emedicare/DocAppointment_2.java
+++ app/src/main/java/com/example/emedicare/DocAppointment_2.java
@@ -10,5 +10,10 @@ public class DocAppointment_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_appointment2);
+
+        String Test_name = getIntent().getStringExtra("Test_name");
+        String Doc_name = getIntent().getStringExtra("Doc_name");
+        String Hospital_name = getIntent().getStringExtra("Hospital_name");
+
     }
 }
