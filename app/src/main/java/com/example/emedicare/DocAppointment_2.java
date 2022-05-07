821777c32fd197c6c5e43bbb4945bc8f3c080646
ImalshaRC <chaminduimalsha@gmail.com>
2022-05-07 13:04:52 +0530

    commit

--- app/src/main/java/com/example/emedicare/DocAppointment_2.java
+++ app/src/main/java/com/example/emedicare/DocAppointment_2.java
@@ -7,8 +7,7 @@ import android.widget.TextView;
 
 public class DocAppointment_2 extends AppCompatActivity {
 
-    TextView Test_Name, Doc_Name;
-    String hospital;
+    TextView appoTitle, docTitle, availableHospital;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +17,14 @@ public class DocAppointment_2 extends AppCompatActivity {
         String Test_name = getIntent().getStringExtra("Test_name");
         String Doc_name = getIntent().getStringExtra("Doc_name");
         String Hospital_name = getIntent().getStringExtra("Hospital_name");
+
+        appoTitle = findViewById(R.id.appoTitle);
+        docTitle = findViewById(R.id.docTitle);
+        availableHospital = findViewById(R.id.availableHospital);
+
+        appoTitle.setText(Test_name);
+        docTitle.setText(Doc_name);
+        availableHospital.setText(Hospital_name);
 
     }
 }
